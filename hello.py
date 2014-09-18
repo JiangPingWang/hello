24188132e3fb4d0c035e06e7cdbd4d55565ad4e0
JiangPingWang <ojumpin@gmail.com>
2014-09-18 09:21:52 +0800

    second.

--- hello.py
+++ hello.py
@@ -5,11 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
 
-@app.route('/name')
-def hi():
-    return "hello, %s" % "王江平"
+    return 'Hello NICE!'
+
 
 if __name__ == '__main__':
     app.run()
