dc0858cf1b396a5d4d52f14153c4f360a299413a
JiangPingWang <ojumpin@gmail.com>
2014-09-18 12:00:25 +0800

    add templates

--- hello.py
+++ hello.py
@@ -1,16 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    return render_template("index.html")
 
-    return 'Hello NICE!'
 
-@app.route('/name')
-def print_name():
-    return '不错，你很厉害！'
+@app.route('/user/<name>')
+def print_name(name):
+    return render_template("user.html", name=name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
